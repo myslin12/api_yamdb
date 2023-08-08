3cb62328c296b3c9bfc1818b5271411881d8f6a5
Nikita Pankratov <rzkn@ya.ru>
2023-08-09 00:13:03 +0300

    Создал приложение titles, и прописал в настройках

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'titles',
 ]
 
 MIDDLEWARE = [
@@ -85,9 +86,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
